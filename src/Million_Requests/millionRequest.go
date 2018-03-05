09183d17f8e58e6fdad78453fe268d08fe05ed7c
edcismybrother <18271683969@163.com>
2018-03-05 19:38:49 +0800

    练习---皮智文

--- src/Million_Requests/millionRequest.go
+++ src/Million_Requests/millionRequest.go
@@ -1,10 +1,29 @@
-package main 
+package main
 
 import (
-
+	"fmt"
 )
 
-func main() {
-	
+type student struct {
+    Name string
+    Age  int
 }
 
+func pase_student() {
+    m := make(map[string]*student)
+    stus := []student{
+        {Name: "zhou", Age: 24},
+        {Name: "li", Age: 23},
+        {Name: "wang", Age: 22},
+    }
+    for _, stu := range stus {
+        m[stu.Name] = &stu
+        fmt.Println(&stu.Age)
+        fmt.Println(&stu.Name)
+    }
+
+}
+
+func main(){
+	pase_student()
+}
